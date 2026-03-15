bb7c86de1727fc704f82706aa6fcf5a771b0fcfd
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-03-15 22:51:21 +0300

    fix: authorization check and reorder the logic

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -2,11 +2,14 @@ import { db } from "../config/db.js";
 import jwt from "jsonwebtoken";
 
 export const askQuestion = async (req, res) => {
-  const { title, question, userId, token } = req.body;
+  const { title, question, token } = req.body;
 
   try {
-    if (!token) {
-      return res.status(400).json({ message: "token missing" });
+    if (!token || !title || !question) {
+      return res.status(400).json({
+        message:
+          "either the token, title or question is missing from the provided information",
+      });
     }
 
     if (title.length > 255) {
@@ -15,13 +18,30 @@ export const askQuestion = async (req, res) => {
         .json({ message: "the title can not be more than 255 chars" });
     }
 
+    if (question.length > 5000) {
+      return res
+        .status(400)
+        .json({ message: "maximum question character limit passed" });
+    }
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+    const userId = decode.userId;
+
+    const cleanTitle = title.trim();
+    const cleanQuestion = question.trim();
+
     await db.execute(
       "INSERT INTO questions(user_id, title, question) VALUES (?,?,?)",
-      [userId, title, question],
+      [userId, cleanTitle, cleanQuestion],
     );
-    res.status(200).json({ message: "opration sucessfull" });
+    res
+      .status(201)
+      .json({ message: "question sumbitted sueccessfully sucessfull" });
   } catch (err) {
     console.log(err);
+    if (err.name === "JsonWebTokenError" || err.name === "TokenExpiredError") {
+      return res.status(401).json({ message: "UNAUTORIZED USER" });
+    }
     res.status(500).json({ message: "INTERNAL SERVER ERROR" });
   }
 };
