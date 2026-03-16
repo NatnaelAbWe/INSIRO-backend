5f41a6340c5981234ed3e97ee05e8c1801dbe0ad
Natnael Abnew <natnaelanbewpro@gmail.com>
2026-03-16 17:34:26 +0300

    feat: fetch with a spacific question route and controller

--- routes/questionRoute.js
+++ routes/questionRoute.js
@@ -3,6 +3,7 @@ import { askQuestion } from "../controllers/questionController.js";
 import { authMiddleware } from "../middlewares/authMiddleware.js";
 import { rateLimit } from "express-rate-limit";
 import { getAllQuestions } from "../controllers/questionController.js";
+import { fetchSpacificQuestion } from "../controllers/questionController.js";
 
 const questionLimiter = rateLimit({
   windowMs: 24 * 60 * 60 * 1000,
@@ -17,5 +18,6 @@ const router = express.Router();
 
 router.post("/ask", authMiddleware, questionLimiter, askQuestion);
 router.get("/all", getAllQuestions);
+router.get("/:questionId", fetchSpacificQuestion);
 
 export default router;
